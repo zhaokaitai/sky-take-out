08f640513a4763b02c5898d83980273970e930b5
赵开泰 <3012579700@qq.com>
2024-08-12 20:18:07 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -90,7 +90,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 		employee.setUpdateTime(LocalDateTime.now());
 		
 		// 设置当前记录创建人id和修改人id
-		// TODO 后期需要改为当前登录用户的id
 		employee.setCreateUser(BaseContext.getCurrentId());
 		employee.setUpdateUser(BaseContext.getCurrentId());
 		
